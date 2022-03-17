e6814340b3a8f4988d4729a1d64b5ee290bbd98b
nikm1 <nikm1@NO1MORE>
2022-03-18 01:08:20 +0200

    Добавьте файлы проекта.

---
+++ 2lab/MainWindow.xaml.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Books2
+{
+    public enum Theme
+    {
+        English, Russian, History, Chemisty
+    }
+    public partial class MainWindow : Window
+    {
+        Themes facultet = new Themes();
+        public MainWindow()
+        {
+            InitializeComponent();
+
+        }
+
+        private void History_Checked(object sender, RoutedEventArgs e)
+        {
+            Catalog.Text = "Format: Name_Student,Sudent_number,Facultet." + '\n';
+            for (int i = 0; i < 10; i++)
+            {
+                if (facultet.Theme_str[i] == Convert.ToString(Theme.History))
+                    Catalog.Text += facultet.Name_Students[i] + ", " + Convert.ToString(i + 1) + ", " + facultet.Theme_str[i] + "." + '\n';
+            }
+        }
+
+        private void Ok_Click(object sender, RoutedEventArgs e)
+        {
+            Catalog.Text = "Format: Name_Student,Sudent_number,Facultet." + '\n';
+            for (int i = 0; i < 10; i++)
+            {
+                Catalog.Text += facultet.Name_Students[i] + ", " + Convert.ToString(i + 1) + ", " + facultet.Theme_str[i] + "." + '\n';
+            }
+        }
+
+        private void Russian_Checked(object sender, RoutedEventArgs e)
+        {
+            Catalog.Text = "Format: Name_Student,Sudent_number,Facultet." + '\n';
+            for (int i = 0; i < 10; i++)
+            {
+                if (facultet.Theme_str[i] == Convert.ToString(Theme.Russian))
+                    Catalog.Text += facultet.Name_Students[i] + ", " + Convert.ToString(i + 1) + ", " + facultet.Theme_str[i] + "." + '\n';
+            }
+        }
+
+        private void Chemisty_Checked(object sender, RoutedEventArgs e)
+        {
+            Catalog.Text = "Format: Name_Student,Sudent_number,Facultet." + '\n';
+            for (int i = 0; i < 10; i++)
+            {
+                if (facultet.Theme_str[i] == Convert.ToString(Theme.Chemisty))
+                    Catalog.Text += facultet.Name_Students[i] + ", " + Convert.ToString(i + 1) + ", " + facultet.Theme_str[i] + "." + '\n';
+            }
+        }
+
+        private void English_Checked(object sender, RoutedEventArgs e)
+        {
+            Catalog.Text = "Format: Name_Student,Sudent_number,Facultet." + '\n';
+            for (int i = 0; i < 10; i++)
+            {
+                if (facultet.Theme_str[i] == Convert.ToString(Theme.English))
+                    Catalog.Text += facultet.Name_Students[i] + ", " + Convert.ToString(i + 1) + ", " + facultet.Theme_str[i] + "." + '\n';
+            }
+        }
+    }
+}
---
+++ 2lab/questions.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Books2
+{
+    public class Themes
+    {
+        public int n;
+        public string[] Name_Students;
+        public string[] Nabor = {  };
+        public string[] Theme_str;
+        public Theme facultet;
+        public Random random;
+        public Themes()
+        {
+            n = 15;
+            Name_Students = new string[n];
+            Theme_str = new string[n];
+            random = new Random();
+            int k = 0;
+            for (int i = 0; i < Nabor.Length; i++)
+            {
+                for (int j = k; j < k + 1; j++)
+                {
+                    Name_Students[j] = Nabor[i];
+                }
+                k += 1;
+            }
+            int themename;
+            for (int i = 0; i < Theme_str.Length; i++)
+            {
+                themename = random.Next(4);
+                Theme theme = (Theme)themename;
+                Theme_str[i] = Convert.ToString(theme);
+            }
+        }
+    }
+}
